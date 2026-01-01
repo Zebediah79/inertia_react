aa9a10cc270ca450c63c771826e7ff4682d7e755
Zebediah Allton <zebediah.allton@gmail.com>
2025-12-31 18:40:16 -0600

    Updated and added MVC files.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return inertia('Home');
-});
+// Route::get('/', function () {
+//     sleep(2);
+//     return Inertia::render('Home', ['name' => 'Zeb']);
+// });
+
+// Route::get('/about', function(){
+//     return Inertia::render('About/About');
+// });
+
+Route::get('/', [PostController::class, 'index']);
+
+Route::resource('posts', PostController::class)->except('index');
